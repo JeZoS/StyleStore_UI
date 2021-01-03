4c3a47bc184f1a34cdbd694021c6da3e098cf1f7
JeZoS <prabhat262111@gmail.com>
2021-01-03 23:11:42 +0530

    kk

---
+++ super-stylist/src/components/Main/mainpage.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { Route, BrowserRouter, Switch, Link, Redirect } from "react-router-dom";
+import SlideShow from "../home files/slideshow";
+import "./Home.css";
+import Home from "../Home";
+import MenCard from "../options/menCard";
+
+class MainPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <>
+        <header className="deskstop-container">
+          <nav className="navb">
+            <div
+              className="options_Login"
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+              }}
+            >
+              <div className="navbar-bran">
+                <span className="super" style={{}}>
+                  <h2>SuperStylist</h2>
+                </span>
+              </div>
+              <div
+                className="all_links"
+                style={{
+                  display: "flex",
+                  flexDirection: "row-reverse",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <Link className="profile" to="/profile">
+                  <div>
+                    <i className="fas fa-user"></i>
+                  </div>
+                </Link>
+                <Link className="Login" to="/login">
+                  <h4>Login/SignUp</h4>
+                </Link>
+
+                <Link className="About" to="/About">
+                  <h4>About Us</h4>
+                </Link>
+                <Link className="Contact" to="/Contact">
+                  <h4>Contact</h4>
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </header>
+
+        <div className="options" style={{ display: "flex" }}>
+          <div>
+            <Link
+              className="Men"
+              to="/Men"
+              onMouseOver={this.props.handleMouseHoverMen}
+              onMouseLeave={this.props.handleMouseHoverMen}
+            >
+              Men
+            </Link>
+
+            <Link
+              className="Women"
+              to="/Women"
+              onMouseOver={this.props.handleMouseHoverWomen}
+              onMouseLeave={this.props.handleMouseHoverWomen}
+            >
+              Women
+            </Link>
+            <Link className="LookBook" to="/LookBook">
+              LookBook
+            </Link>
+            <Link className="FashionTips" to="/FashionTips">
+              FashionTips
+            </Link>
+          </div>
+          <div style={{ marginRight: "10px" }}>
+            <Link className="Home" to="/">
+              Home
+            </Link>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+export default MainPage;
